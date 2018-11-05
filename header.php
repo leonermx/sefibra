46fea4a3a06568b25a6f5db7a1404705cc0f4047
leonermx <leonerhd@gmail.com>
2018-11-05 00:30:28 -0600

    header

---
+++ header.php
@@ -0,0 +1,86 @@
+<html><!-- Preloader -->
+    <div id="fakeloader"></div>
+	
+	<div class="top-menu-1x ">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 ">
+					<div class="top-menu-left">
+						<p></p>
+						<b><i class="fa fa-phone"></i>  (55) 5544-5836</b>
+						<b><i class="fa fa-envelope"></i>  sistemaempresarialfibra@gmail.com</b>
+					</div>				
+				</div>				
+				<div class="col-md-6">
+					<div class="top-menu-right">
+						<div class="footer-info-right">
+							<ul>
+								<li><a href="#"> <i class="fa fa-facebook"></i> </a></li>										
+								<li><a href="#"> <i class="fa fa-twitter"></i> </a></li>											
+								<li><a href="#"> <i class="fa fa-google"></i> </a></li>									
+								<li><a href="#"> <i class="fa fa-linkedin"></i> </a></li>											
+							</ul>					
+						</div>					
+					</div>				
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="bussiness-main-menu-1x">	
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">		
+					<div class="business-main-menu">		
+						<nav class="navbar navbar-expand-lg navbar-light bg-light btco-hover-menu">
+							<a class="navbar-brand" href="index.html">
+								<img src="images/logo-dark.png" class="d-inline-block align-top" alt="">						
+							</a>
+						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						  </button>
+
+						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						  
+							<ul class="navbar-nav ml-auto business-nav">
+
+							  <li class="nav-item">
+									<a class="nav-link dropdown-item" href="nosotros.html">
+										Nosotros</a>
+								
+							  </li>
+							  <li class="nav-item dropdown">
+									<a class="nav-link dropdown-item" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										Productos <i class="fa fa-angle-down"></i>
+									</a>
+									  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
+											<li><a class="dropdown-item" href="portfolio-one.html">Projects One</a></li>
+											<li><a class="dropdown-item" href="portfolio-two.html">Projects Two</a></li>
+											<li><a class="dropdown-item" href="project-details.html">Projects Details</a></li>
+										</ul>
+								
+							  </li>
+
+							  <li class="nav-item dropdown">
+									<a class="nav-link dropdown-item" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										Servicios <i class="fa fa-angle-down"></i>
+									</a>
+									
+							  </li>	
+
+							 					 
+							  
+							  <li class="nav-item">
+								<a class="nav-link dropdown-item" href="contact-us.html">Contacto</a>
+							  </li>		  
+							
+							
+						  
+							
+						  </div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	<html>
--- index.php
+++ index.php
@@ -33,8 +33,9 @@
     <link href="style.css" rel="stylesheet">
     <!-- Default CSS Color --> 
     <link href="color/default.css" rel="stylesheet">
-     <!-- Color CSS --> 
-  
+		 <!-- Color CSS --> 
+		 
+		 <script src="csi.js-master/src/csi.js"></script>
 	<!-- Responsive CSS -->
     <link href="css/responsive.css" rel="stylesheet">
 
@@ -42,87 +43,8 @@
   </head>
   <body>
   
-   <!-- Preloader -->
-    <div id="fakeloader"></div>
-	
-	<div class="top-menu-1x ">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 ">
-					<div class="top-menu-left">
-						<p></p>
-						<b><i class="fa fa-phone"></i>  (55) 5544-5836</b>
-						<b><i class="fa fa-envelope"></i>  sistemaempresarialfibra@gmail.com</b>
-					</div>				
-				</div>				
-				<div class="col-md-6">
-					<div class="top-menu-right">
-						<div class="footer-info-right">
-							<ul>
-								<li><a href="#"> <i class="fa fa-facebook"></i> </a></li>										
-								<li><a href="#"> <i class="fa fa-twitter"></i> </a></li>											
-								<li><a href="#"> <i class="fa fa-google"></i> </a></li>									
-								<li><a href="#"> <i class="fa fa-linkedin"></i> </a></li>											
-							</ul>					
-						</div>					
-					</div>				
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="bussiness-main-menu-1x">	
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">		
-					<div class="business-main-menu">		
-						<nav class="navbar navbar-expand-lg navbar-light bg-light btco-hover-menu">
-							<a class="navbar-brand" href="index.html">
-								<img src="images/logo-dark.png" class="d-inline-block align-top" alt="">						
-							</a>
-						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						  </button>
-
-						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-						  
-							<ul class="navbar-nav ml-auto business-nav">
-
-							  <li class="nav-item">
-									<a class="nav-link dropdown-item" href="nosotros.html">
-										Nosotros</a>
-								
-							  </li>
-							  <li class="nav-item dropdown">
-									<a class="nav-link dropdown-item" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Productos <i class="fa fa-angle-down"></i>
-									</a>
-								
-							  </li>
-
-							  <li class="nav-item dropdown">
-									<a class="nav-link dropdown-item" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Servicios <i class="fa fa-angle-down"></i>
-									</a>
-									
-							  </li>	
-
-							 					 
-							  
-							  <li class="nav-item">
-								<a class="nav-link dropdown-item" href="contact-us.html">Contacto</a>
-							  </li>		  
-							
-							
-						  
-							
-						  </div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>	
+			
+			<?php include 'cabecera.html'; ?>
 	
 	<div class="business-main-slider">
 		<div class="owl-carousel main-slider">
@@ -338,7 +260,7 @@
 						<div class="app-present-right-2">
 							<div class="single-app-present">
 								<div class="media">
-								  <span class="animatedhover pulse"><i class="fa fa-circle"></i></span>
+								  <span class="animatedhover pulse"><a href="servicios-integrales.html"><i class="fa fa-circle"></i></a></span>
 								  <div class="media-body">
 									<h2>SERVICIOS INTEGRALES</h2>
 									<p>Estos servicios consisten en el análisis técnico exhaustivo para implementar, ampliar o adecuar las redes de trabajo y aplicaciones para las organizaciones..</p>
@@ -402,7 +324,7 @@
 							  <div class="media-body">
 								<h5>Telefonía IP</h5>
 								<p>Terminales, Conmutadores, Racks y Accesorios.</p>
-								<a href="#">Leer más <i class="fa fa-angle-right"></i></a>
+								<a href="telefoniaip.html">Leer más <i class="fa fa-angle-right"></i></a>
 							  </div>
 							</div>	
 						</div>
@@ -615,7 +537,7 @@
 							  <a href="#"><img src="images/client/logo-8.png" alt="slide 1" class=""></a>
 							</div> 
 							<div class="item">
-							  <a href="#"><img src="images/client/logo-9.png" alt="slide 1" class=""></a>
+							  <a href="#"><img src="images/client/logo-9.jpg" alt="slide 1" class=""></a>
 							</div>             
 						</div>	
 					</div>									
